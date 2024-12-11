b6ad70292ba49aab54e0b9ac6a7ee4d4f4740e70
Im54d <147472558+Im54d@users.noreply.github.com>
2024-12-11 13:32:51 +0500

    Delete scientifypublic-c6ecc00b30bdea5e01ecc10e230e98d47695ae0d directory

--- scientifypublic-c6ecc00b30bdea5e01ecc10e230e98d47695ae0d/scripts/local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PORT=8080
-export DATABASE_URL="postgres://postgres:123@localhost:5432/scientify?sslmode=disable"
-go run main.go 
--- scientifypublic-c6ecc00b30bdea5e01ecc10e230e98d47695ae0d/static/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.tooltip').forEach(item => {
-    item.addEventListener('mouseover', event => {
-        console.log(`Tooltip: ${event.target.getAttribute('data-tooltip')}`);
-    });
-});
